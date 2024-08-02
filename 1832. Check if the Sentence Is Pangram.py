f8c71a25feb4151c1b9cbd88a4be7e749c149645
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-08-02 10:55:17 +0500

    updated solution

--- 1832. Check if the Sentence Is Pangram.py
+++ 1832. Check if the Sentence Is Pangram.py
@@ -1,12 +1,5 @@
 def checkIfPangram(sentence):
-    alphabet = 'qwertyuiopasdfghjklzxcvbnm'
-    s = set(sentence)
-
-    for i in range(len(alphabet)):
-        if not alphabet[i] in s:
-            return False
-    
-    return True
+    return len(set(sentence)) == 26
 
 sentence = "thequickbrownfoxjumpsoverthelazydog"
 print(checkIfPangram(sentence))
