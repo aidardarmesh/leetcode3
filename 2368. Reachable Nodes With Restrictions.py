df2b37c7db4e4b09f0f85fb30006166995346825
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-08-06 20:24:22 +0500

    Tree: DFS. Connected Components

---
+++ 2368. Reachable Nodes With Restrictions.py
@@ -0,0 +1,22 @@
+def reachableNodes(n, edges, restricted):
+    from collections import defaultdict
+
+    graph = defaultdict(list)
+    seen = set(restricted)
+    start = 0
+
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+
+    def dfs(node, graph, seen):
+        seen.add(node)
+
+        ans = 1
+        for n in graph[node]
+            if not n in seen:
+                ans += dfs(n, graph, seen)
+        
+        return ans
+    
+    return dfs(start)
