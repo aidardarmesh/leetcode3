0ac1ea11295cb571bb374c2a20aedf01283c337c
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-08-27 18:03:47 +0500

    Linked List: Reverse

---
+++ 0206. Reverse Linked List.py
@@ -0,0 +1,10 @@
+def reverseList(head):
+    prev, curr = None, head
+
+    while curr:
+        next_node = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_node
+    
+    return prev
