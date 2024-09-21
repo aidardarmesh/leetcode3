a5db3b6d3651012dfa9981b4fc51b6e9bec0e64a
Aidar Darmesh <darmesh.aidar@gmail.com>
2024-09-21 21:25:05 +0500

    Binary Search

--- 0035. Search Insert Position.py
+++ 0035. Search Insert Position.py
@@ -1,17 +1,19 @@
 def search_insert(nums, target):
     left, right = 0, len(nums)-1
+    ans = -1
 
+    # find position of first greater than target element
+    # if not found -> should be last element
     while left <= right:
         mid = (left + right) // 2
 
-        if nums[mid] < target:
-            left = mid + 1
-        elif target < nums[mid]:
+        if nums[mid] >= target:
+            ans = mid
             right = mid - 1
         else:
-            return mid
+            left = mid + 1
     
-    return left
+    return ans if ans != -1 else len(nums)
 
 print(search_insert([1,3,5,6], 5))
 print(search_insert([1,3,5,6], 2))
